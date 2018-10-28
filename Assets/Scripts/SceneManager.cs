f2b14a62880dd089bad2216b9b2cd541278592b6
vitaly <vitaly.malishchuk@gmail.com>
2018-10-28 23:24:50 +0200

    add scenes

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour {
+    public enum Scenes {
+        MainMenu,
+        Playground,
+        ResultTable
+    }
+
 
 	// Use this for initialization
 	void Start () {
