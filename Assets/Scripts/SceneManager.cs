787ab152aaef0e1340a04d3ba0516bdbe8138d1f
vitaly <vitaly.malishchuk@gmail.com>
2018-10-28 23:27:59 +0200

    sceneMAnager mod

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour {
+    public enum Scenes {
+        MainMenu,
 
+    }
 	// Use this for initialization
 	void Start () {
 		
